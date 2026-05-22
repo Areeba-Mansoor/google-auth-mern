8be2a40b3fff43a9bef61742e1391f3ea45cf9ff
Areeba Mansoor <159564271+Areeba-Mansoor@users.noreply.github.com>
2026-05-22 22:04:25 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ passport.use(
     new googleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:5000/auth/google/callback'
+        callbackURL: 'https://google-auth-alpha.vercel.app/auth/google/callback'
     },
         (accessToken, refreshToken, profile, done) => {
 
